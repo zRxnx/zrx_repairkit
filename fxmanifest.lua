1ac60d8583ba446243ef3576135d589d016a34aa
zRxnx <rene.kovacs0712@gmail.com>
2024-06-21 20:28:51 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,7 +5,7 @@ use_experimental_fxv2_oal 'yes'
 
 author 'zRxnx'
 description 'Advanced repairkit system'
-version '2.3.1'
+version '2.3.2'
 
 docs 'https://docs.zrxnx.at'
 discord 'https://discord.gg/mcN25FJ33K'
